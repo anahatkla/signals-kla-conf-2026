c10f6a3b31e67a1a247e7b2d0f435704a3356363
Anthony Nahat <a.nahat@kunzleigh.com>
2026-02-14 13:43:55 -0500

    to observable

--- src/app/signal-form/signal-form.component.ts
+++ src/app/signal-form/signal-form.component.ts
@@ -8,6 +8,8 @@ import {form, FormField, max, min, required} from '@angular/forms/signals';
 import {DateUtility} from '../date.utility';
 import {DataService} from '../data.service';
 import {FormErrorsComponent} from '../form-errors/form-errors.component';
+import {toObservable} from '@angular/core/rxjs-interop';
+import {tap} from 'rxjs';
 
 @Component({
   selector: 'app-signal-form',
@@ -31,7 +33,7 @@ import {FormErrorsComponent} from '../form-errors/form-errors.component';
 })
 export class SignalFormComponent {
   protected readonly DataService = DataService;
-  protected readonly age = computed(() => DateUtility.calculateAge(this.form.dateOfBirth().value()))
+  protected readonly breedOptions = computed(() => DataService.breeds[this.form.species().value()]);
   private readonly formDataModel = signal({
     name: "",
     dateOfBirth: new Date(),
@@ -46,12 +48,13 @@ export class SignalFormComponent {
     max(schemaPath.minimumTreats, context => context.valueOf(schemaPath.maximumTreats), {message: "Minimum Treats must be less than Maximum Treats"});
     min(schemaPath.maximumTreats, context => context.valueOf(schemaPath.minimumTreats), {message: "Maximum Treats must be greater than Minimum Treats"});
   });
-  protected readonly breedOptions = computed(() => DataService.breeds[this.form.species().value()]);
+  protected readonly age = computed(() => DateUtility.calculateAge(this.form.dateOfBirth().value()))
 
   constructor() {
-    effect(() => {
-      this.form.species().value();
-      this.form.breed().setControlValue("");
-    });
+    toObservable(this.form.species().value)
+      .pipe(
+        tap(() => this.form.breed().setControlValue(""))
+      )
+      .subscribe();
   }
 }
