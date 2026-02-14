cadfac7b052f928d63c029927009fa2ec63358ea
Anthony Nahat <a.nahat@kunzleigh.com>
2026-02-14 15:17:54 -0500

    demo 4 and 5

--- src/app/demo-5/demo-5.component.ts
+++ src/app/demo-5/demo-5.component.ts
@@ -1,11 +1,30 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, effect, signal, WritableSignal} from '@angular/core';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-demo-5',
-  imports: [],
+  imports: [
+    MatButton
+  ],
   templateUrl: './demo-5.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Demo5Component {
+  protected readonly $count = signal(1);
+  protected readonly $multiplier = signal(1);
+  protected readonly $result = signal(0);
 
+  constructor() {
+    effect(() => {
+      if (this.$multiplier()) {
+        this.$result.set(this.$count() * this.$multiplier());
+        // this.$count.update(x => x + 1);
+      }
+      console.log("Effect run")
+    })
+  }
+
+  increment(signal: WritableSignal<number>): void {
+    signal.update(x => x + 1);
+  }
 }
